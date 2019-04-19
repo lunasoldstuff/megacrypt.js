5f11ee2f07978734c6794d435fbb4dd32c9ec015
Robert Wilcox <42777835+stampylongr@users.noreply.github.com>
2019-04-19 14:23:34 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     // set if domain without port is use as host
-  domain: false,
+  domain: true,
     // Set Host
-  host: '127.0.0.1',
+  host: 'megacryptjs.herokuapp.com',
     // The port megacrypt.js will be running on
-  port: '3427',
+  port: '',
     // Server Decryption Key (change this if hosted)
   serverKey: "PrkqcWVp@/-,R%2P/kEWC-'PgPMHFFM@>EF4x5J-5]}=-:N%rm${dVLf5@3F]/=2",
     // if true, the link encryption tool will on the base url
