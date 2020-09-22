56c0e8b3fba617fec42d6bdcd659de6bce198c6c
Luna <63075513+lunaisnotaboy@users.noreply.github.com>
2020-09-22 10:59:38 -0400

    Update sentry.js

--- routes/sentry.js
+++ routes/sentry.js
@@ -3,8 +3,8 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/dothething', function (req, res, next) {
-  res.statusCode = 500;
-  res.end(res.sentry + "\n");
+  res.render('sentry')
+  throw new Error("somebody visited the page~!");
 })
 
 module.exports = router
